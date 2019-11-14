f9e239c13f15662386af29287d34d02b06f1a99f
s-lockema <s-lockema@53S500038087.it.bsd405.org>
2019-11-14 08:47:31 -0800

    Committing checkpoint 1

--- src/main/java/fracCalc/FracCalc.java
+++ src/main/java/fracCalc/FracCalc.java
@@ -23,18 +23,29 @@ public class FracCalc {
     }
 
     public static String produceAnswer(String equation) {
-//    	If there are spaces left to do (meaning that the equation is fully
+//    	if there are spaces left to do (meaning that the equation is fully
 //    	parsed and solved, it will exit the while loop
     	while (equation.indexOf(' ') != -1) {
+//    		finds first operand by taking a substring of all chars before
+//    		first space
     		String operand1 = equation.substring(0, equation.indexOf(' '));
         	
+//    		deletes the first operand from equation
         	equation = equation.substring(equation.indexOf(' ') + 1);
         	
+//        	finds operator, which will be single character at the beginning
+//        	of string
         	char operator = equation.charAt(0);
         	
+//        	deletes the operator from the equation
         	equation = equation.substring(2);
         	String operand2;
         	
+//        	if there are still more operators left to parse out, it will
+//        	find the next operand by taking a substring of all chars
+//        	before the next space. if there are no more operators left to
+//        	parse, the second operand will be the rest of the equation
+//        	the second operand is then deleted from the equation
         	if (equation.indexOf(' ') != -1) {
         		operand2 = equation.substring(0, equation.indexOf(' '));
         		equation = equation.substring(equation.indexOf(' ')); 
@@ -43,218 +54,22 @@ public class FracCalc {
         		equation = "";
         	}
         	
-        	equation = operate(operand1, operator, operand2) + equation;
+        	return operand2;
+        	
+//        	the result calculated by operand 1 and 2, and put back at the
+//        	beginning of equation; if another operation still exists,
+//        	while loop will repeat 
+//        	equation = operate(operand1, operator, operand2) + equation;
     	}
     	
     	return equation;
-    	//return operand2;
         
-    	//return "whole:" + stringToWhole(operand2)
-        //     + " numer:" + stringToNumer(operand2)
-        //     + " denom:" + stringToDenom(operand2);
+//    	return "whole:" + stringToWhole(operand2)
+//             + " numer:" + stringToNumer(operand2)
+//             + " denom:" + stringToDenom(operand2);
     	
     }
-    
-    public static int stringToWhole(String input) {
-    	String whole;
-    	if (input.indexOf('_') != -1) {
-    		whole = input.substring(0, input.indexOf('_'));
-    	} else if (input.indexOf('_') == -1 && input.indexOf('/') == -1) {
-    		whole = input;
-    	} else {
-    		whole = "0";
-    	}
-    	
-    	return Integer.parseInt(whole);
-    	
-    }
-    
-    public static int stringToNumer(String input) {
-    	String numer;
-    	if (input.indexOf('/') >= 0) {
-    		numer = input.substring(input.indexOf('_') + 1, input.indexOf('/'));
-    	} else {
-    		numer = "0";
-    	}
-    	
-    	if (stringToWhole(input) >= 0) {
-    		return Integer.parseInt(numer);
-    	} else {
-    		return -1 * Integer.parseInt(numer);
-    	}
-    	
-    }
-    
-    public static int stringToDenom(String input) {
-    	String denom;    	
-    	if (input.indexOf('/') >= 0) {
-    		denom = input.substring(input.indexOf('/') + 1);
-    	} else {
-    		denom = "1";
-    	}
-    	
-    	return Integer.parseInt(denom);
-    }
-    
-    public static String add(String operand1, String operand2) {
-		int whole1 = stringToWhole(operand1);
-    	int numer1 = stringToNumer(operand1);
-    	int denom1 = stringToDenom(operand1);
-    	int whole2 = stringToWhole(operand2);
-    	int numer2 = stringToNumer(operand2);
-    	int denom2 = stringToDenom(operand2);
-    	
-    	numer1 = improperNumerResult(whole1, numer1, denom1);
-    	numer2 = improperNumerResult(whole2, numer2, denom2);
-    	
-    	int numerResult = (numer1 * denom2) + (numer2 * denom1);
-    	int denomResult = denom1 * denom2;
-    	
-    	int gcm = gcm(numerResult, denomResult);
-    	
-    	numerResult /= gcm;
-    	denomResult /= gcm;
-    	
-    	int wholeResult = improperToMixedWholeResult(numerResult, denomResult);
-    	int excessNumer = wholeResult * denomResult;
-    	numerResult = numerResult - excessNumer;
-    	
-    	return intsToOperand(wholeResult, numerResult, denomResult);
-	}
-    
-    public static String subtract(String operand1, String operand2) {
-		int whole1 = stringToWhole(operand1);
-    	int numer1 = stringToNumer(operand1);
-    	int denom1 = stringToDenom(operand1);
-    	int whole2 = stringToWhole(operand2);
-    	int numer2 = stringToNumer(operand2);
-    	int denom2 = stringToDenom(operand2);
-    	
-    	numer1 = improperNumerResult(whole1, numer1, denom1);
-    	numer2 = improperNumerResult(whole2, numer2, denom2);
-    	
-    	int numerResult = (numer1 * denom2) - (numer2 * denom1);
-    	int denomResult = denom1 * denom2;
-    	
-    	int gcm = gcm(numerResult, denomResult);
-    	
-    	numerResult /= gcm;
-    	denomResult /= gcm;
-    	
-    	int wholeResult = improperToMixedWholeResult(numerResult, denomResult);
-    	int excessNumer = wholeResult * denomResult;
-    	numerResult = numerResult - excessNumer;
-    	
-    	return intsToOperand(wholeResult, numerResult, denomResult);
-	}
-    
-    public static String multiply(String operand1, String operand2) {
-		int whole1 = stringToWhole(operand1);
-    	int numer1 = stringToNumer(operand1);
-    	int denom1 = stringToDenom(operand1);
-    	int whole2 = stringToWhole(operand2);
-    	int numer2 = stringToNumer(operand2);
-    	int denom2 = stringToDenom(operand2);
-	
-    	numer1 = improperNumerResult(whole1, numer1, denom1);
-    	numer2 = improperNumerResult(whole2, numer2, denom2);
-    	
-    	int numerResult = numer1 * numer2;
-    	int denomResult = denom1 * denom2;
-    	
-    	int gcm = gcm(numerResult, denomResult);
-    	numerResult /= gcm;
-    	denomResult /= gcm;
-    	
-    	int wholeResult = improperToMixedWholeResult(numerResult, denomResult);
-    	int excessNumer = wholeResult * denomResult;
-    	numerResult = numerResult - excessNumer;
-    	
-    	return intsToOperand(wholeResult, numerResult, denomResult);
-	}
-    
-    public static String divide(String operand1, String operand2) {
-		int whole1 = stringToWhole(operand1);
-    	int numer1 = stringToNumer(operand1);
-    	int denom1 = stringToDenom(operand1);
-    	int whole2 = stringToWhole(operand2);
-    	int numer2 = stringToNumer(operand2);
-    	int denom2 = stringToDenom(operand2);
-	
-    	numer1 = improperNumerResult(whole1, numer1, denom1);
-    	numer2 = improperNumerResult(whole2, numer2, denom2);
-    	
-    	int numerResult = numer1 * denom2;
-    	int denomResult = numer2 * denom1;
-    	
-    	int gcm = gcm(numerResult, denomResult);
-    	numerResult /= gcm;
-    	denomResult /= gcm;
-    	
-    	int wholeResult = improperToMixedWholeResult(numerResult, denomResult);
-    	int excessNumer = wholeResult * denomResult;
-    	numerResult = numerResult - excessNumer;
-    	
-    	return intsToOperand(wholeResult, numerResult, denomResult);
-	}
-    
-    public static int improperNumerResult(int whole, int numer, int denom) {
-    	int excess = whole * denom;
-    	numer += excess;
-    	return numer;
-    }
-    
-    public static int improperToMixedWholeResult(int numer, int denom) {
-    	int whole = numer / denom;
-    	return whole;
-    }
-    
-    public static int gcm(int numer, int denom) {
-    	numer = Math.abs(numer);
-    	denom = Math.abs(denom);    	
-    	int dividend = 2;
-    	int gcm = 1;
-    	while (dividend <= numer) {
-    		if (numer % dividend == 0 && denom % dividend == 0) {
-    			gcm *= dividend;
-    			numer /= dividend;
-    			denom /= dividend;	
-    		} else {
-    			dividend++;
-    		}
-    	}
-    	
-    	return gcm;
-    }
-    
-    public static String intsToOperand(int whole, int numer, int denom) {
-    	if (numer == 0) {
-    		return whole + "";
-    	} else if (whole == 0) {
-    		if (denom > 0) {
-    			return numer + "/" + denom;
-    		} else {
-    			return (-1 * numer) + "/" + Math.abs(denom);
-    		}
-    	} else {
-    		return whole + "_" + (Math.abs(numer))+ "/" + (Math.abs(denom));
-    	}
-    }
-    
-    public static String operate(String operand1, char operator, String operand2) {
-    	if (operator == '+') {
-    		return (add(operand1, operand2));
-    	} else if (operator == '-') {
-    		return (subtract(operand1, operand2));
-    	} else if (operator == '*') {
-    		return (multiply(operand1, operand2));
-    	} else if (operator == '/') {
-    		return (divide(operand1, operand2));
-    	} else {
-    		return "ERROR: Invalid operator.";
-    	}
-    }
-    
+
     public static boolean toContinue(String equation) {
     	equation = equation.toLowerCase();
     	if (equation.charAt(0) == 'q' && equation.charAt(1) == 'u' &&
